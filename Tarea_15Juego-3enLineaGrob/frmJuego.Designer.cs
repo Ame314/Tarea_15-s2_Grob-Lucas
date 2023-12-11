a3d8a3e004034e9d644775e2ea74b3c010728975
Ame314 <amelie.grobrosero@gmail.com>
2023-12-11 09:01:20 -0500

    Agregar archivos de proyecto.

---
+++ Tarea_15Juego-3enLineaGrob/frmJuego.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Tarea_15Juego_3enLineaGrob
+{
+    partial class frmJuego
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.txtJugador = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bienvenido al juego 3 en línea ";
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(37, 56);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(75, 23);
+            this.btnIniciar.TabIndex = 1;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // txtJugador
+            // 
+            this.txtJugador.Location = new System.Drawing.Point(235, 29);
+            this.txtJugador.Name = "txtJugador";
+            this.txtJugador.ReadOnly = true;
+            this.txtJugador.Size = new System.Drawing.Size(390, 20);
+            this.txtJugador.TabIndex = 2;
+            this.txtJugador.Text = "Tu turno #.";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(37, 100);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(637, 328);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Juego en proceso";
+            // 
+            // frmJuego
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtJugador);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.label1);
+            this.Name = "frmJuego";
+            this.Text = "Tres en línea ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.TextBox txtJugador;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
---
+++ Tarea_15Juego-3enLineaGrob/frmJuego.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tarea_15Juego_3enLineaGrob
+{
+    public partial class frmJuego : Form
+    {
+        PictureBox[,] imagenes;
+        bool turno;
+        int[,] matriz = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+        public frmJuego()
+        {
+            InitializeComponent();
+        }
+        private void Jugar_Click(object sender, EventArgs e)
+        {
+            string nombre = (sender as PictureBox).Name;
+            string[] aux = nombre.Split('_');
+            int fila = Convert.ToInt32(aux[1]);
+            int col= Convert.ToInt32(aux[2]);
+
+            if (turno)
+            {
+
+                imagenes[fila, col].Image = Tarea_15Juego_3enLineaGrob.Properties.Resources.Estrella;
+            }
+            else
+            {
+                imagenes[fila, col].Image = Tarea_15Juego_3enLineaGrob.Properties.Resources.Circulo;
+            }
+               
+           
+            imagenes[fila, col].Enabled = false;//no cambia 
+
+            turno = !turno;
+            QuienJuega();
+            
+        }
+        private void QuienJuega()
+        {  
+          if (turno)
+            this.txtJugador.Text = "tu turno jugador #1";
+          else 
+            this.txtJugador.Text = "tu turno jugador #2";
+
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            turno = true;
+            EnceraMatriz();
+            QuienJuega();
+            
+                
+            imagenes = new PictureBox[3, 3];
+            int x;
+            int y= 25;
+
+            for (int i = 0; i<imagenes.GetLength(0); i++)
+            {
+               x= 19;
+                
+                for(int j = 0; j<imagenes.GetLength(1); j++)
+                {
+                    imagenes[i, j] = new PictureBox();
+                    imagenes[i, j].Size = new Size(147, 105);
+                    imagenes[i, j].Location = new Point(x, y);
+                    imagenes[i, j].Image = Tarea_15Juego_3enLineaGrob.Properties.Resources.vacio;
+                    imagenes[i, j].SizeMode = PictureBoxSizeMode.StretchImage;
+                    imagenes[i, j].Name = $"MisImagenes_{i}_{j}";
+                    imagenes[i, j].Click += new System.EventHandler(this.Jugar_Click);
+
+
+                    this.groupBox1.Controls.Add(imagenes[i,j]);
+                    x += 147;
+                }
+                y += 107;
+            }
+
+            this.btnIniciar.Enabled = false;
+
+        }
+        private void EnceraMatriz()
+        {
+            for(int i = 0;i<imagenes.GetLength(0);i++)
+            {
+                for( int j = 0; j<matriz.GetLength(1); j++)
+                {
+                    matriz[i, j] = 0;
+                    //matriz = new int[3][] = { { 0, 0, 0 },{ 0, 0, 0},{ 0, 0, 0} };
+                }
+            }
+        }
+        private void ImprimirMatriz()
+        {
+            for (int i = 0; i < matriz.GetLength(0); i++)
+                for (int j = 0; j < matriz.GetLength(1);)
+                    Console.Write($"matriz[i,j]");
+        }
+
+        /*
+         if (turno)
+        imagenes[filas, col].Image = Tarea_15Juego_3enLineaGrob.Properties.Recurces.circulo;
+        matriz
+
+    }
+}
